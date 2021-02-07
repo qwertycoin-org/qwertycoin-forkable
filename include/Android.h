0f90537e60ede058884f7fae75cb66b805ad7b25
ExploShot <exploshot@qwertycoin.org>
2021-02-07 13:22:20 +0100

    Execute Coding Conventions in include/Android.h

--- include/Android.h
+++ include/Android.h
@@ -14,30 +14,30 @@ namespace std {
 template <typename T>
 string to_string(T value)
 {
-    std::ostringstream os;
-    os << value;
+	std::ostringstream os;
+	os << value;
 
-    return os.str();
+	return os.str();
 }
 
 /*
 inline unsigned long stoul (std::string const &str, size_t *idx = 0, int base = 10)
 {
-    char *endp;
-    unsigned long value = strtoul(str.c_str(), &endp, base);
-    if (endp == str.c_str()) {
-        throw std::invalid_argument("my_stoul");
-    }
+	char *endp;
+	unsigned long value = strtoul(str.c_str(), &endp, base);
+	if (endp == str.c_str()) {
+		throw std::invalid_argument("my_stoul");
+	}
 
-    if (value == ULONG_MAX && errno == ERANGE) {
-        throw std::out_of_range("my_stoul");
-    }
+	if (value == ULONG_MAX && errno == ERANGE) {
+		throw std::out_of_range("my_stoul");
+	}
 
-    if (idx) {
-        *idx = endp - str.c_str();
-    }
+	if (idx) {
+		*idx = endp - str.c_str();
+	}
 
-    return value;
+	return value;
 }
 */
 } // namespace std
